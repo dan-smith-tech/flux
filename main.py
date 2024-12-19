03fec44223618049fe080947d7194eb4b736b698
Dan Smith <dev@dansmith.tech>
2024-12-19 15:11:52 +0000

    Refactor model inferencing to return image buffer and instead save it in main

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ Entry point of the program.
 """
 
 from input import get_guidance_scale, get_image_size, get_prompt
-from model import get_image
+from model import generate_image
 
 
 def init():
     """
-    Collects user input and generates an image accordingly.
+    Collect user input and generate an image accordingly.
     """
 
     prompt = get_prompt()
     width, height = get_image_size()
     guidance_scale = get_guidance_scale()
-    get_image(prompt, width, height, guidance_scale)
-    print("Image generated.")
+
+    output_image = generate_image(prompt, width, height, guidance_scale)
+    output_image.save("output.png")
+    print("Image generated at `output.png`.")
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -6,22 +6,22 @@ import torch
 from diffusers import FluxPipeline
 
 
-def get_image(prompt, width, height, guidance_scale):
-    # load the image generator model
+def generate_image(prompt, width, height, guidance_scale):
+    """
+    (Down)Load the Flux and generate an image based on the provided prompt and settings.
+    """
+
+    # (down)load the image generator model
     pipe = FluxPipeline.from_pretrained(
         "black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16
     )
 
-    # run the model
-    images = pipe(
+    # run the model and return the generated image
+    return pipe(
         prompt,
         width=width,
         height=height,
         guidance_scale=guidance_scale,
         num_inference_steps=50,
         max_sequence_length=512,
-    ).images
-
-    # save each image to a file
-    for i, image in enumerate(images):
-        image.save(f"output-{i}.png")
+    ).images[0]
